aa96c9fc2c4edbdb918c7ffdb10f8a4c722a0c13
Calvin Roberts <roberts.calvin85@gmail.com>
2021-08-02 23:11:22 +0100

    Create active class function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const banner = document.getElementsByClassName('.banner')[0],
-  blocks = document.getElementsByClassName('.blocks');
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
 for (var i = 1; i < 400; i++) {
   banner.innerHTML += "<div class='blocks'></div>";
   const duration = Math.random() * 5;
   blocks[i].style.animationDuration = 2 + duration + 's';
   blocks[i].style.animationDelay = duration + 's';
 }
+
+const section = document.querySelector('section');
+setTimeout(function () {
+  section.classList.add('active');
+}, 14000); // add class active after 14seconds
